31f29feac18ccfe93c6ccf9045dd766a005a08de
gromoff97 <gromoff97@mail.ru>
2018-01-06 18:49:40 +0300

    DefTranslator-class was created

--- makena_translator.php
+++ makena_translator.php
@@ -1,3 +1,14 @@
 <?php
 
+/**
+* This is a class which is created for translating websites (special for "MAKENA")
+*/
+class DefTranslator 
+{
+	function __construct($UrlName)
+	{
+		# code...
+	}
+}
+
 ?>
